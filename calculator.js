de408f4d778b9f166ae4fd91fb92bed712128388
Aidos <tastanbek03012000@gmail.com>
2023-10-22 16:11:08 +0600

    first init

---
+++ calculator.js
@@ -0,0 +1,52 @@
+let count=0;
+let a,b;
+document.getElementById('num1Btn').onclick = function() {
+    count = 1
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num2Btn').onclick = function() {
+    count = 2
+    ocument.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num3Btn').onclick = function() {
+    count = 3
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num4Btn').onclick = function() {
+    count = 4
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num5Btn').onclick = function() {
+    count = 5
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num6Btn').onclick = function() {
+    count = 6
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num7Btn').onclick = function() {
+    count = 7
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num8Btn').onclick = function() {
+    count = 8
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num9Btn').onclick = function() {
+    count = 9
+    document.getElementById('countLabel').innerText = count
+}
+
+document.getElementById('num0Btn').onclick = function() {
+    count = 0
+    document.getElementById('countLabel').innerText = count
+}
+
